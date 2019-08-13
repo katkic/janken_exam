5889f22ab08495b12fed14cee124ed39f4642851
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-08-13 11:06:21 +0900

    first commit

---
+++ sample.rb
@@ -0,0 +1,27 @@
+class Player
+  def hand
+    # コンソールを入力待ち状態にし、
+    # プレイヤーがコンソールから打ち込んだ値を出力する処理のメソッドの処理をこの中に作成する
+  end
+end
+
+class Enemy
+  def hand
+    # グー、チョキ、パーの値をランダムに出力するメソッドの処理をこの中に作成する
+  end
+end
+
+class Janken
+  def pon(player_hand, enemy_hand)
+    # プレイヤーが打ち込んだ値と、Enemyがランダムに出した値でじゃんけんをさせ、
+    # その結果をコンソール上に出力するメソッドをこの中に作成する
+    # その際、あいこもしくはグー、チョキ、パー以外の値入力時には、もう一度ジャンケンをする
+    # 相手がグー、チョキ、パーのうち、何を出したのかも表示させる
+  end
+end
+
+player = Player.new
+enemy = Enemy.new
+janken = Janken.new
+
+janken.pon(player.hand, enemy.hand)
