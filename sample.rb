4f8532281846c3e5c2e7b8724abcfe8a4c570738
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-08-13 14:03:58 +0900

    Jankenクラスのponメソッドを実装

--- sample.rb
+++ sample.rb
@@ -1,23 +1,42 @@
 class Player
   def hand
-    puts '数字を入力してください', '0: グー', '1: チョキ', '2: パー'
-    gets.to_i
+    puts '数字を入力してください', '0: グー', '1: チョキ', '2: パー', ''
+    puts '最初はグー、ジャンケン...', ''
+    gets.chomp
   end
 end
 
 class Enemy
   def hand
-    rand(3)
+    rand(3).to_s
   end
 end
 
 class Janken
-  def pon(player_hand, enemy_hand)
-    # プレイヤーが打ち込んだ値と、Enemyがランダムに出した値でじゃんけんをさせ、
-    # その結果をコンソール上に出力するメソッドをこの中に作成する
-    # その際、あいこもしくはグー、チョキ、パー以外の値入力時には、もう一度ジャンケンをする
-    # 相手がグー、チョキ、パーのうち、何を出したのかも表示させる
+  def pon(player, enemy)
+    hands = { '0' => 'グー', '1' => 'チョキ', '2' => 'パー' }
+    player_hand = ''
+    enemy_hand = ''
 
+    loop do
+      player_hand = player.hand
+      enemy_hand = enemy.hand
+      puts "player: #{hands[player_hand]}", "enemy: #{hands[enemy_hand]}"
+
+      if !(player_hand == '0' || player_hand == '1' || player_hand == '2')
+        puts '不正な値が入力されました。0〜2の数字を入力してください', ''
+      elsif player_hand == enemy_hand
+        puts 'あいこです、もう一回！', ''
+      else
+        break
+      end
+    end
+
+    if (player_hand.to_i - enemy_hand.to_i + 3) % 3 == 2
+      puts 'あなたの勝ちです！'
+    else
+      puts 'あなたの負けです...'
+    end
   end
 end
 
@@ -25,6 +44,4 @@ player = Player.new
 enemy = Enemy.new
 janken = Janken.new
 
-# player.hand
-p enemy.hand
-# janken.pon(player.hand, enemy.hand)
+janken.pon(player, enemy)
